fc3379ec26e17cdf0ee3263bb282b694104cc784
BestBall <bestball@BestBalls-MacBook-Air.local>
2022-10-27 01:27:51 +0700

    feat:add feature register and posts

--- server/app.js
+++ server/app.js
@@ -1,8 +1,28 @@
 import express from "express";
 import postsRouter from "./apps/postsRouter.js";
+import bodyParser from "body-parser";
+import { pool } from "./utils/db.js";
 
 const app = express();
 const port = 4000;
+
+app.use(bodyParser.json());
+
+app.post("/register", async (req, res) => {
+  //console.log(req.body);
+  const userId = req.body.user_id;
+  const username = req.body.username;
+  const password = req.body.password;
+  const firstname = req.body.firstName;
+  const lastname = req.body.lastName;
+  await pool.query(
+    "INSERT INTO users (user_id, username, password, first_name,last_name) VALUES ($1,$2,$3,$4,$5);",
+    [userId, username, password, firstname, lastname]
+  );
+  return res.json({
+    message: "Created a new User successfully",
+  });
+});
 
 app.use("/posts", postsRouter);
 
--- server/apps/postsRouter.js
+++ server/apps/postsRouter.js
@@ -1,11 +1,38 @@
 import { Router } from "express";
+import { pool } from "../utils/db.js";
 
 const postsRouter = Router();
 
-postsRouter.post("/", (req, res) => {
-  console.log(req);
+postsRouter.post("/", async (req, res) => {
+  console.log(req.body);
+  const postId = req.body.post_id;
+  const title = req.body.post_title;
+  const content = req.body.post_content;
+  const likes = req.body.post_likes;
+  const videoUrl = req.body.post_video_url;
+  const imageUrl = req.body.post_image_url;
+  const createdAt = new Date();
+  const updatedAt = new Date();
+  const categoryId = req.body.category_id;
+  const userId = req.body.user_id;
+
+  await pool.query(
+    "INSERT INTO posts(post_id,post_title,post_content,post_likes,post_video_url,post_image_url,created_at,updated_at,category_id,user_id) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10)",
+    [
+      postId,
+      title,
+      content,
+      likes,
+      videoUrl,
+      imageUrl,
+      createdAt,
+      updatedAt,
+      categoryId,
+      userId,
+    ]
+  );
   return res.json({
-    message: "Create a new user successfully",
+    message: "Created a new Post successfully",
   });
 });
 
